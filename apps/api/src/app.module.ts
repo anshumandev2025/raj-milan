1d54b17ea9f8f32a4195634ef918720c385388b2
anshumandev2002 <anshumandev2002@gmail.com>
2025-05-20 21:04:52 +0530

    Feat: auth api

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { DatabaseModule } from './config/database.module';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    UserModule,
+    DatabaseModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ apps/api/src/user/schema/user.schema.ts
@@ -0,0 +1,112 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema({ timestamps: true })
+export class User extends Document {
+  @Prop({ isRequired: true })
+  fullName: string;
+
+  @Prop({ isRequired: true })
+  gender: string;
+
+  @Prop({ isRequired: true, unique: true })
+  emailAddress: string;
+
+  @Prop({ isRequired: true, unique: true })
+  mobileNumber: string;
+
+  @Prop({ isRequired: true })
+  location: string;
+
+  @Prop({ isRequired: true })
+  subCast: string;
+
+  @Prop({ isRequired: true })
+  password: string;
+
+  @Prop()
+  dataOfBirth: string;
+
+  @Prop()
+  height: string;
+
+  @Prop()
+  educationLevel: string;
+
+  @Prop()
+  degreeOrSpecialialization: string;
+
+  @Prop()
+  jobTitleOrDesignation: string;
+
+  @Prop()
+  companyOrOrganization: string;
+
+  @Prop()
+  anualIncome: string;
+
+  @Prop()
+  dietPreference: string;
+
+  @Prop()
+  smokingHabit: string;
+
+  @Prop()
+  drinkingHabit: string;
+
+  @Prop()
+  aboutHobbyOrInterset: string;
+
+  @Prop()
+  fatherOccupation: string;
+
+  @Prop()
+  motherOccupation: string;
+
+  @Prop()
+  siblingsCount: string;
+
+  @Prop()
+  familyType: string;
+
+  @Prop()
+  familyValues: string;
+
+  @Prop()
+  aboutFamilyBackground: string;
+
+  @Prop()
+  partnerPreferedMinAge: number;
+
+  @Prop()
+  partnerPreferedMaxAge: number;
+
+  @Prop()
+  partnerPreferedMinHeight: string;
+
+  @Prop()
+  partnerPreferedMaxHeight: string;
+
+  @Prop()
+  partnerPreferedSubCast: string;
+
+  @Prop()
+  partnerPreferedCity: string;
+
+  @Prop()
+  partnerPreferedEducationLevel: string;
+
+  @Prop()
+  partnerPreferedProfession: string;
+
+  @Prop()
+  partnerAdditionalPreference: string;
+
+  @Prop()
+  profileViews: number;
+
+  @Prop()
+  profileCompletedPercent: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
